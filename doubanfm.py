a5027aa8a64630ba642efa0da65cb0ff7886b56a
Andy Scout <andyhuzhill@gmail.com>
2014-04-06 22:17:11 +0800

    修改：      doubanfm.py 	修改：      doubanfm.pyc 	修改：      index.py 	修改：      templates/about.html 	修改：      templates/contact.html 	修改：      templates/index.html 	修改：      templates/signin.html

--- doubanfm.py
+++ doubanfm.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
-# *-* encoding: utf-8 *-*
-#
+# *-* encoding: utf-8 *-* #
 # =============================================
 #      Author   : Andy Scout
 #    Homepage   : http://andyhuzhill.github.com
@@ -21,6 +20,8 @@ import ConfigParser
 from cookielib import CookieJar
 import subprocess
 
+child = subprocess.Popen
+
 def saveToFile(fileName, content):
     file = open(fileName, 'wb')
     file.write(content)
@@ -29,9 +30,11 @@ def saveToFile(fileName, content):
 def getPlayList(channel='0', opener=None):
     url = 'http://douban.fm/j/mine/playlist?type=n&channel=' + channel
     if opener == None:
-        return json.loads(urllib2.urlopen(url).read())
+        playlist = json.loads(urllib2.urlopen(url).read())
+        return playlist
     else:
-        return json.loads(opener.open(urllib2.Request(url)).read())
+        playlist = json.loads(opener.open(urllib2.Request(url)).read())
+        return playlist
 
 def signIn(username, password):
     opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(CookieJar()))
@@ -58,6 +61,62 @@ def signIn(username, password):
             print('Login success!')
             return opener
 
+def getVerifyCode():
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(CookieJar()))
+    captcha_id = opener.open(urllib2.Request(
+        'http://douban.fm/j/new_captcha')).read().strip('"')
+    saveToFile('static/verify_code.jpg',
+             opener.open(urllib2.Request('http://douban.fm/misc/captcha?size=m&id=' + captcha_id)).read())
+    return captcha_id
+
+def SignIn(username, passwd, verifycode_id, verifycode):
+    print verifycode_id
+    print verifycode
+    opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(CookieJar()))
+    response = json.loads(opener.open(
+        urllib2.Request('http://douban.fm/j/login'),
+        urllib.urlencode({
+            'source': 'radio',
+            'alias' : username,
+            'form_password': passwd,
+            'captcha_solution': verifycode,
+            'captcha_id' : verifycode_id,
+            'task':'sync_channel_list'})).read())
+    if 'err_msg' in response.keys():
+        print(response['err_msg'])
+        return False, None
+    else:
+        print('Login success!')
+        return True, opener
+
+
+def getListLength(playlist):
+    cnt = 0
+    for item in playlist:
+        cnt += 1
+    return cnt
+
+def getNItemOfList(playlist, num):
+    cnt = 0
+    for item in playlist:
+        cnt += 1
+        if cnt == num:
+            return item
+
+def playSongOfList(playlist, num=0):
+    global child
+    if num > getListLength(playlist['song']):
+        return False, None, None
+    else:
+        song = getNItemOfList(playlist['song'], num) 
+        print song
+        try:
+            child.kill()
+            child = subprocess.Popen(['mpg123', song['url']])
+        except Exception, e:
+            child = subprocess.Popen(['mpg123', song['url']])
+        return True, song, child
+
 def play(channel='0', opener=None):
     while True:
         if opener == None:
@@ -66,7 +125,6 @@ def play(channel='0', opener=None):
             playlist = getPlayList(channel, opener)
 
         if playlist['song'] == []:
-            print("Get playlist failed!")
             break
 
         for song in playlist['song']:
--- index.py
+++ index.py
@@ -14,16 +14,41 @@
 
 import tornado.web
 import tornado.ioloop
+import time
 
 from doubanfm import *
 
+i = 1
+
+g_opener = None
+
+playlist = getPlayList("1", g_opener) 
+
 class MainHandler(tornado.web.RequestHandler):
     def get(self):
-        playlist = getPlayList("0", None)
-        if playlist["song"] == []:
-            self.render("index.html", err_msg = u"无法获取歌曲列表")
+        global playlist
+        global g_opener
+        global i
+        if playlist['song'] == []:
+            self.render("msg.html", msg = u"无法获取歌曲列表!")
         else:
-            self.render("index.html", err_msg = None, songs = playlist["song"])
+            if i > getListLength(playlist['song']):
+                playlist = getPlayList("1", g_opener)
+                i = 1
+            success, song, child = playSongOfList(playlist, i)
+            if success:
+                self.render("index.html", songs=playlist['song'], imgsrc = song['picture'], songtitle=song['title'], singer=song['artist']) 
+
+    def post(self):
+        global i
+        global playlist
+        global g_opener
+        i += 1
+        if i >  getListLength(playlist['song']):
+            playlist = getPlayList("1", g_opener)
+            i = 1
+        success, song, child = playSongOfList(playlist, i)
+        self.render("index.html", songs=playlist['song'], imgsrc= song['picture'], songtitle=song['title'], singer=song['artist']) 
 
 
 class AboutHandler(tornado.web.RequestHandler):
@@ -35,11 +60,28 @@ class ContactHandler(tornado.web.RequestHandler):
         self.render("contact.html")
 
 class SigninHandler(tornado.web.RequestHandler):
+    verifycode_id = ""
     def get(self):
+        global verifycode_id
+        verifycode_id = getVerifyCode()
+        print verifycode_id
         self.render("signin.html")
 
     def post(self):
-        self.write(self.get_argument("password"))
+        global verifycode_id
+        global g_opener
+        user = self.get_argument("user")
+        passwd = self.get_argument("passwd")
+        verifycode = self.get_argument("verifycode")
+        success, g_opener = SignIn(user, passwd, verifycode_id, verifycode)
+        if success:
+            self.render("msg.html", msg = u"登陆成功！")
+            time.sleep(1)
+            self.redirect("/")
+        else:
+            self.render("msg.html", msg = u"登陆失败！")
+            time.sleep(1)
+            self.redirect("/")
 
 app = tornado.web.Application([
     (r"/", MainHandler),
